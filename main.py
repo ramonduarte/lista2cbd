87d616e0a8a9ff2bf091e2e0ddfb961c843753f2
Ramon Melo <ramonduarte@poli.ufrj.br>
2018-12-12 20:19:55 -0200

    Heap testbed.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import heapq
 import sqlparse
 from typing import List
 from datetime import datetime
+from timeit import default_timer as timer
 
 
 with open('consulta_cand_2018/consulta_cand_2018_BR.csv',
@@ -51,21 +52,31 @@ class HeapTable(list):
 
     def __init__(self, list_of_objs: list, parameters: List[int] = [0]):
         list_of_objs = [HeapRecord(x, parameters) for x in list_of_objs]
+        heapq.heapify(list_of_objs)
         super().__init__(list_of_objs)
 
+    def append(self, value):
+        return heapq.heappush(self, value)
+
+    def pop(self, index=-1):
+        if index == 0:
+            return super().pop(0)
+        return heapq.heappop(self)
 
 
 #%%
 class DBFile(object):
     data = None
     header = None
+    schema_map = {}
     struct = ""
     schema = ""
+    record_file = ""
 
     def __init__(self, schema_file: str):
-        schema_str = ""
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
         try:
+            schema_str = ""
+            now = datetime.now().strftime("%Y%m%d%H%M%S")
             f = open(file=schema_file, mode="r", encoding="latin-1")
             file_content = f.readlines()
             if not file_content:
@@ -76,6 +87,11 @@ class DBFile(object):
                 + ["Modification date: {}".format(now) + "\n"]
                 + [file_content[-1]])
             f.close()
+
+            for x in file_content[-1].replace("Schema: ",
+                                              "").replace(")", "").split(";"):
+                if x:
+                    self.schema_map[x.split(":")[0]] = int(x.split(":")[1].split("(")[1])
         except OSError:
             schema_str = "File structure: {}\n" \
             "Creation date: {}\n" \
@@ -85,6 +101,16 @@ class DBFile(object):
             f = open(file=schema_file, mode="w", encoding="latin-1")
             f.write(schema_str)
             f.close()
+
+
+        try:
+            f = open(file=self.record_file, mode="w", encoding="latin-1")
+            for x in self.data:
+                f.write(";".join(x) + "\n")
+            f.close()
+        except OSError:
+            pass
+
 
     def select(self, statement):
         raise NotImplementedError
@@ -99,13 +125,14 @@ class DBFile(object):
         raise NotImplementedError
 
     def get_column(self, fieldname: str) -> List[str]:
-        index = self.header.index(fieldname)
+        index = self.header.index(fieldname.strip())
         column = [x[index] for x in self.data]
         return column
 
 
 class HeapDBFile(DBFile):
     struct = "Heap"
+    record_file = "heapfile.csv"
 
     def __init__(self, filename: str, schema_file: str,
                  parameters: List[int] = [15]):
@@ -130,17 +157,135 @@ class HeapDBFile(DBFile):
         return False
 
     def select(self, statement):
-        columns = str(statement[0]).split(",")
+        columns = str(statement[1]).split(",")
         tables = str( (statement[2:] + [None])[0] )
-        where = str( (statement[3:] + [None])[0] ).split(" and ")
+        where = str( (statement[3:] + [None])[0] ).lower().replace("where ", "").replace(";", "").split(" and ")
+        for w in where:
+            # TODO: parse "=", "in" and "between"
+            break
 
-        return zip(*[self.get_column(x) for x in self.header])
+        if columns[0] == '*':
+            return zip(*[self.get_column(x) for x in self.header])
+        else:
+            return zip(*[self.get_column(x.upper()) for x in columns])
+
+    def insert(self, statement):
+        tables = str(statement[1])
+        columns = str(statement[2]).upper().strip().replace("(", "").replace(")", "").split(",")
+        values = str(statement[4]).upper().strip().replace("(", "").replace(")", "").split(",")
+
+        assert len(columns) == len(values)
+        new_record = [None] * len(self.header)
+        for column in range(len(self.header)):
+            if column in columns:
+                i = self.header.index(column)
+                j = columns.index(column)
+                new_record[i] = values[j]
+        
+        self.data.append(HeapRecord(new_record, self.data.sort_parameters))
+
+        return True
+
+    def delete(self, statement):
+        tables = str(statement[1])
+        columns = str(statement[1]).split(",")
+        where = str(statement[2]).split(" and ")
+        # where = str( (statement[2:] + [None])[0] ).split(" and ")
+
+        if columns[0] == '*':
+            return zip(*[self.get_column(x) for x in self.header])
+        else:
+            return zip(*[self.get_column(x.upper()) for x in columns])
 
 
+## HEAP TESTBED
+# load heap and write to file
+counter = 0
+start = timer()
+new_heap = HeapDBFile("consulta_cand_2018/consulta_cand_2018_BR.csv", "HeapHEAD.txt")
+print(counter, timer() - start)
 
-new_heap = HeapDBFile("consulta_cand_2018/consulta_cand_2018_BR.csv", "test.txt")
-result = new_heap.parse("select shdnslkjdhn")
-for x in result:
-    for y in x:
-        print(y, end=",")
-    print("\n")
+# SELECT statements
+counter += 1
+start = timer()
+new_heap.parse("select * from table_name where SQ_CANDIDATO = 280000622172);")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("select * from table_name where SQ_CANDIDATO in (280000624082, 280000624083, 280000624085, 280000624086, 280000625869, 280000625870, 280000629807, 280000629807, 280000629808, 280000629808;")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("select * from table_name where SQ_CANDIDATO between 280000600000 and 280000700000;")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("select * from table_name where NM_PARTIDO = PODEMOS and DS_ESTADO_CIVIL = CASADO(A);")
+print(counter, timer() - start)
+
+
+# INSERT statements
+counter += 1
+start = timer()
+with open("insert1.txt") as f:
+    sql = f.readlines()
+for statement in sql:
+    new_heap.parse(statement)
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+with open("insert10.txt") as f:
+    sql = f.readlines()
+for statement in sql:
+    new_heap.parse(statement)
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+with open("insert10.txt") as f:
+    sql = f.readlines()
+for statement in sql:
+    new_heap.parse(statement)
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+with open("insert10.txt") as f:
+    sql = f.readlines()
+for statement in sql:
+    new_heap.parse(statement)
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+with open("insertall.txt") as f:
+    sql = f.readlines()
+for statement in sql:
+    new_heap.parse(statement)
+print(counter, timer() - start)
+
+
+# DELETE statements
+counter += 1
+start = timer()
+new_heap.parse("delete from table_name where SQ_CANDIDATO = 280000622172;")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("delete from table_name where SQ_CANDIDATO in (280000624082, 280000624083, 280000624085, 280000624086, 280000625869, 280000625870, 280000629807, 280000629807, 280000629808, 280000629808);")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("delete from table_name where SQ_CANDIDATO between 280000600000 and 280000700000;")
+print(counter, timer() - start)
+
+counter += 1
+start = timer()
+new_heap.parse("delete from table_name where NM_PARTIDO = PODEMOS and DS_ESTADO_CIVIL = CASADO(A);")
+print(counter, timer() - start)
